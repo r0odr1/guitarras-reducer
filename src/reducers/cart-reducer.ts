3bf9a949a3e0de63134edf30ff628f1faa943bc2
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-26 09:52:44 -0500

    agregando reducer

---
+++ src/reducers/cart-reducer.ts
@@ -0,0 +1,10 @@
+import type { Guitar } from "../types";
+
+
+export type CartActions =
+  { type: 'add-to-cart', payload: {item: Guitar} } |
+  { type: 'remove-from-cart', payload: {id: Guitar['id']} } |
+  { type: 'decrease-quantity', payload: {id: Guitar['id']} } |
+  { type: 'increase-quantity', payload: {id: Guitar['id']} } |
+  { type: 'cleare-cart' }
+  
